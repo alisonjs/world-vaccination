dd8dd73e4fdd96e878458f2f49090b2e402d402a
Alison Sousa <alison.sousa@ufrn.br>
2021-10-13 10:33:17 -0300

    Unit test exception cases

--- world-vaccination-business/src/test/java/UserServiceImplTest.java
+++ world-vaccination-business/src/test/java/UserServiceImplTest.java
@@ -1,11 +1,13 @@
 import com.alisonjs.business.domain.User;
+import com.alisonjs.business.exceptions.AuthorizationException;
+import com.alisonjs.business.exceptions.BusinessException;
+import com.alisonjs.business.exceptions.NotFoundException;
 import com.alisonjs.business.repository.UserRepository;
 import com.alisonjs.business.service.UserService;
 import com.alisonjs.business.service.impl.UserServiceImpl;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -74,6 +76,61 @@ public class UserServiceImplTest {
         }
     }
 
+    @Nested
+    class throwExceptionWhen{
+
+        @Test
+        void userIdNotFound(){
+            Mockito.lenient().when(userRepository.getOne(ArgumentMatchers.anyLong())).thenReturn(null);
+            var exception = Assertions.assertThrows(NotFoundException.class, () -> userService.getOne(1L), "User not found with the given id");
+            Assertions.assertNotNull(exception);
+            Assertions.assertEquals("User not found with the given id 1", exception.getMessage());
+            Mockito.verify(userRepository, Mockito.times(1)).getOne(ArgumentMatchers.anyLong());
+        }
+
+        @Test
+        void usernameAndPasswordNotFound(){
+            Mockito.lenient().when(userRepository.getByUsernameAndPassword(ArgumentMatchers.anyString(), ArgumentMatchers.anyString())).thenReturn(null);
+            var exception = Assertions.assertThrows(AuthorizationException.class, () -> userService.login("user", "pass"), "User or password incorrect.");
+            Assertions.assertNotNull(exception);
+            Assertions.assertEquals("User or password incorrect.", exception.getMessage());
+            Mockito.verify(userRepository, Mockito.times(1)).getByUsernameAndPassword(ArgumentMatchers.anyString(), ArgumentMatchers.anyString());
+        }
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        void usernameRequiredFieldNotFound(String username){
+            var user = getUserInstance();
+            user.setUsername(username);
+            var exception = Assertions.assertThrows(BusinessException.class, () -> userService.save(user));
+            Assertions.assertNotNull(exception);
+            Assertions.assertEquals("Username required", exception.getMessage());
+            Mockito.verify(userRepository, Mockito.times(0)).save(ArgumentMatchers.any(User.class));
+        }
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        void passwordRequiredFieldNotFound(String password){
+            var user = getUserInstance();
+            user.setPassword(password);
+            var exception = Assertions.assertThrows(BusinessException.class, () -> userService.save(user));
+            Assertions.assertNotNull(exception);
+            Assertions.assertEquals("Password required", exception.getMessage());
+            Mockito.verify(userRepository, Mockito.times(0)).save(ArgumentMatchers.any(User.class));
+        }
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        void emailRequiredFieldNotFound(String email){
+            var user = getUserInstance();
+            user.setEmail(email);
+            var exception = Assertions.assertThrows(BusinessException.class, () -> userService.save(user));
+            Assertions.assertNotNull(exception);
+            Assertions.assertEquals("Email required", exception.getMessage());
+            Mockito.verify(userRepository, Mockito.times(0)).save(ArgumentMatchers.any(User.class));
+        }
+
+    }
 
     public User getUserInstance(){
         return User.builder()
